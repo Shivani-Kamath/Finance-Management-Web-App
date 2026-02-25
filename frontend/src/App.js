5e3171f6ba91729caa115d9371bea1139d3cb994
Shivani-Kamath <shivanikamath369@gmail.com>
2026-02-25 20:12:56 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import ProtectedRoute from './components/ProtectedRoute'; // import this
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Navigate to="/login" />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route
+          path='/home'
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
